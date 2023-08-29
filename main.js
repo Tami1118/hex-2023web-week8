32b80ae91a49b4c2889c87564ca675f1ee09bb11
Tami1118 <chenziyi93326@gmail.com>
2023-08-29 14:10:15 +0800

    :tada: Bulid: bulid environment

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ import './node_modules/bootstrap/dist/js/bootstrap.min';
 import './node_modules/jquery/dist/jquery.min.js';
 import AOS from 'aos';
 import './node_modules/aos/dist/aos.css';
+// import Swiper from 'swiper';
+// import 'swiper/css';
+
+// const swiper = new Swiper(...);
+
 
 AOS.init();
