cf06184d5e76d33f616c10d3d8a4b875409d76a4
Tami1118 <chenziyi93326@gmail.com>
2023-08-29 12:11:50 +0800

    :tada: Bulid: bulid environment
    Bulid: bulid environment

---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/scss/all.scss';
+import './node_modules/bootstrap/dist/js/bootstrap.min';
+import './node_modules/jquery/dist/jquery.min.js';
+import AOS from 'aos';
+import './node_modules/aos/dist/aos.css';
+
+AOS.init();
